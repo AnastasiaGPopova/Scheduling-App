01c163356a7bf2244855f6c3d680ad9d081d6eb1
unknown <ana.popova.register@gmail.com>
2024-06-19 13:59:10 +0300

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false);
+const connect = async () => {
+  try {
+    mongoose.connect(
+      "mongodb+srv://AniUSER:AniTest@clustermngapp.6xytfwx.mongodb.net/",{}
+    );
+    console.log(`Connected to MongoDB`)
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+
+//-----Adding middleware-------
+//Always! it returns a middleware which parse the url encoded body, this will be used for every request
+
+app.listen(3030, () => {
+  connect()
+  console.log(`The server is running on Port 3030...`);
+});
+
+
+
+
